dd69f70a73f792acbeffa883186139d14339d31f
Mikko Lorico <mikkodlorico@gmail.com>
2026-02-28 10:37:22 -0800

    Last minute default plan changed

--- sfu-degree-planner/src/data/defaultPlan.js
+++ sfu-degree-planner/src/data/defaultPlan.js
@@ -5,10 +5,10 @@ export const DEFAULT_PLAN = {
   "2025-Fall": ["MSE 210", "MSE 220", "MSE 224", "MSE 250", "MSE 281"],
   "2026-Spring": ["MSE 212", "MSE 222", "MSE 251", "MSE 252", "MSE 280"],
   "2026-Summer": ["COOP-1"],
-  "2026-Fall": ["MSE 323", "MSE 310", "MSE 320", "MSE 4XX-1", "CMPL-3"],
+  "2026-Fall": ["MSE 323", "MSE 310", "MSE 320", "CMPL-3"],
   "2027-Spring": ["COOP-2"],
   "2027-Summer": ["MSE 300", "MSE 312", "MSE 321", "MSE 353", "MSE 381"],
   "2027-Fall": ["COOP-3"],
-  "2028-Spring": ["MSE 402", "MSE 410", "MSE 4XX-2", "MSE 4XX-3"],
+  "2028-Spring": ["MSE 402", "MSE 410", "MSE 4XX-2", "MSE 4XX-3", "MSE 4XX-1"],
   "2028-Summer": ["MSE 352", "MSE 405W", "MSE 411", "MSE 4XX-4"]
 };
